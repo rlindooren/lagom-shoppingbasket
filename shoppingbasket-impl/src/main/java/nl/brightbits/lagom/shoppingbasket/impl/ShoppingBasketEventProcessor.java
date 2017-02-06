2aebf8e4d4063b7de4d28c38632f95353f21df9f
rlindooren <ricardolindooren@thesting.com>
2017-02-06 21:34:27 +0100

    Improved storage of shopping basket data in Cassandra
    Was using one table 'shoppingbasket' which basically contained only shopping basket items. But this approach didn't allow to query something like 'get the last known shopping basket of a customer'. Therefore created two tables 'shoppingbasket' and 'shoppingbasket_item' with optimized key structure.

--- shoppingbasket-impl/src/main/java/nl/brightbits/lagom/shoppingbasket/impl/ShoppingBasketEventProcessor.java
+++ shoppingbasket-impl/src/main/java/nl/brightbits/lagom/shoppingbasket/impl/ShoppingBasketEventProcessor.java
@@ -10,10 +10,12 @@ import com.lightbend.lagom.javadsl.persistence.cassandra.CassandraSession;
 import nl.brightbits.lagom.shoppingbasket.impl.ShoppingBasketEvent.ItemAddedToShoppingBasket;
 import nl.brightbits.lagom.shoppingbasket.impl.ShoppingBasketEvent.ItemAmountUpdatedInShoppingBasket;
 import nl.brightbits.lagom.shoppingbasket.impl.ShoppingBasketEvent.ItemRemovedFromShoppingBasket;
+import nl.brightbits.lagom.shoppingbasket.impl.ShoppingBasketEvent.ShoppingBasketCreated;
 import org.pcollections.PSequence;
 import org.pcollections.TreePVector;
 
 import javax.inject.Inject;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletionStage;
@@ -22,12 +24,14 @@ import static com.lightbend.lagom.javadsl.persistence.cassandra.CassandraReadSid
 
 public class ShoppingBasketEventProcessor extends ReadSideProcessor<ShoppingBasketEvent> {
 
-    public static final String TABLE_NAME = "shoppingbasket";
+    public static final String TABLE_NAME_SHOPPINGBASKET = "shoppingbasket";
+    public static final String TABLE_NAME_SHOPPINGBASKET_ITEM = "shoppingbasket_item";
     private final CassandraSession session;
     private final CassandraReadSide readSide;
-    private PreparedStatement insertShoppingBasketItemPs = null;
-    private PreparedStatement updatedShoppingBasketItemPs = null;
-    private PreparedStatement deleteShoppingBasketItemPs = null;
+    private PreparedStatement insertShoppingBasketPs;
+    private PreparedStatement insertShoppingBasketItemPs;
+    private PreparedStatement updatedShoppingBasketItemPs;
+    private PreparedStatement deleteShoppingBasketItemPs;
 
     @Inject
     public ShoppingBasketEventProcessor(CassandraSession session, CassandraReadSide readSide) {
@@ -45,6 +49,7 @@ public class ShoppingBasketEventProcessor extends ReadSideProcessor<ShoppingBask
         return readSide.<ShoppingBasketEvent>builder("shoppingbasket_offset")
                 .setGlobalPrepare(this::prepareCreateTables)
                 .setPrepare((ignored) -> prepareStatements())
+                .setEventHandler(ShoppingBasketCreated.class, this::processShoppingBasketCreated)
                 .setEventHandler(ItemAddedToShoppingBasket.class, this::processItemAddedToShoppingBasket)
                 .setEventHandler(ItemAmountUpdatedInShoppingBasket.class, this::processItemAmountUpdatedInShoppingBasket)
                 .setEventHandler(ItemRemovedFromShoppingBasket.class, this::processItemRemovedFromShoppingBasket)
@@ -53,68 +58,97 @@ public class ShoppingBasketEventProcessor extends ReadSideProcessor<ShoppingBask
 
     private CompletionStage<Done> prepareCreateTables() {
         // @formatter:off
-        return session.executeCreateTable(
-                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+        CompletionStage<Done> createShoppingBasketTable = session.executeCreateTable(
+                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_SHOPPINGBASKET + " (" +
                         "id uuid, " +
                         "shopId text, " +
                         "customerId text, " +
+                        "created timestamp, " +
+                        "PRIMARY KEY ((customerId, shopId), created)" +
+                        ") WITH CLUSTERING ORDER BY (created DESC)");
+
+        CompletionStage<Done> createShoppingBasketItemTable = session.executeCreateTable(
+                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_SHOPPINGBASKET_ITEM + " (" +
+                        "shoppingBasketId uuid, " +
                         "skuId text, " +
                         "amount int, " +
+                        "added timestamp, " +
                         "lastUpdated timestamp, " +
-                        "PRIMARY KEY (id, skuId)" +
+                        "PRIMARY KEY (shoppingBasketId, skuId)" +
                         ")");
         // @formatter:on
+
+        return createShoppingBasketTable
+                .thenCombine(createShoppingBasketItemTable, ((done, done2) -> done));
     }
 
     private CompletionStage<Done> prepareStatements() {
-        CompletionStage<Done> insert = session.prepare("INSERT INTO " + TABLE_NAME + " " +
-                "(id, shopId, customerId, skuId, amount) VALUES (?, ?, ?, ?, ?)")
+        CompletionStage<Done> insertShoppingBasket = session.prepare("INSERT INTO " + TABLE_NAME_SHOPPINGBASKET +
+                " (id, customerId, shopId, created) VALUES (?, ?, ?, ?)")
+                .thenApply(ps -> {
+                    insertShoppingBasketPs = ps;
+                    return Done.getInstance();
+                });
+
+        CompletionStage<Done> insertShoppingBasketItem = session.prepare("INSERT INTO " + TABLE_NAME_SHOPPINGBASKET_ITEM +
+                " (shoppingBasketId, skuId, amount, added) VALUES (?, ?, ?, ?)")
                 .thenApply(ps -> {
                     insertShoppingBasketItemPs = ps;
                     return Done.getInstance();
                 });
 
-        CompletionStage<Done> update = session.prepare("UPDATE " + TABLE_NAME + " " +
-                "SET amount=? where id=? and skuId=?")
+        CompletionStage<Done> updateShoppingBasketItem = session.prepare("UPDATE " + TABLE_NAME_SHOPPINGBASKET_ITEM +
+                " SET amount=?, lastUpdated=? where shoppingBasketId=? and skuId=?")
                 .thenApply(ps -> {
                     updatedShoppingBasketItemPs = ps;
                     return Done.getInstance();
                 });
 
-        CompletionStage<Done> delete = session.prepare("DELETE FROM " + TABLE_NAME + " where id=? and skuId=?")
+        CompletionStage<Done> deleteShoppingBasketItem = session.prepare("DELETE FROM " + TABLE_NAME_SHOPPINGBASKET_ITEM +
+                " where shoppingBasketId=? and skuId=?")
                 .thenApply(ps -> {
                     deleteShoppingBasketItemPs = ps;
                     return Done.getInstance();
                 });
 
-        return insert
-                .thenCombine(update, ((done, done2) -> done))
-                .thenCombine(delete, ((done, done2) -> done));
+        return insertShoppingBasket
+                .thenCombine(insertShoppingBasketItem, ((done, done2) -> done))
+                .thenCombine(updateShoppingBasketItem, ((done, done2) -> done))
+                .thenCombine(deleteShoppingBasketItem, ((done, done2) -> done));
+    }
+
+    private CompletionStage<List<BoundStatement>> processShoppingBasketCreated(ShoppingBasketCreated event) {
+        BoundStatement bindInsertItem = insertShoppingBasketPs.bind()
+                .setUUID("id", UUID.fromString(event.getId()))
+                .setString("shopId", event.getShopId())
+                .setString("customerId", event.getCustomerId())
+                .setTimestamp("created", new Date());
+        return completedStatement(bindInsertItem);
     }
 
     private CompletionStage<List<BoundStatement>> processItemAddedToShoppingBasket(ItemAddedToShoppingBasket event) {
         BoundStatement bindInsertItem = insertShoppingBasketItemPs.bind()
-                .setUUID("id", UUID.fromString(event.getShoppingBasketId()))
-                .setString("shopId", event.getShopId())
-                .setString("customerId", event.getCustomerId())
+                .setUUID("shoppingBasketId", UUID.fromString(event.getShoppingBasketId()))
                 .setString("skuId", event.getSkuId())
-                .setInt("amount", event.getInitialAmount());
+                .setInt("amount", event.getInitialAmount())
+                .setTimestamp("added", new Date());
         return completedStatement(bindInsertItem);
     }
 
     private CompletionStage<List<BoundStatement>> processItemAmountUpdatedInShoppingBasket(
             ItemAmountUpdatedInShoppingBasket event) {
         BoundStatement bindUpdateItem = updatedShoppingBasketItemPs.bind()
-                .setString("id", event.getShoppingBasketId())
+                .setUUID("shoppingBasketId", UUID.fromString(event.getShoppingBasketId()))
                 .setString("skuId", event.getSkuId())
-                .setInt("amount", event.getNewAmount());
+                .setInt("amount", event.getNewAmount())
+                .setTimestamp("lastUpdated", new Date());
         return completedStatement(bindUpdateItem);
     }
 
     private CompletionStage<List<BoundStatement>> processItemRemovedFromShoppingBasket(
             ItemRemovedFromShoppingBasket event) {
         BoundStatement bindDeleteItem = deleteShoppingBasketItemPs.bind()
-                .setString("id", event.getShoppingBasketId())
+                .setString("shoppingBasketId", event.getShoppingBasketId())
                 .setString("skuId", event.getSkuId());
         return completedStatement(bindDeleteItem);
     }
